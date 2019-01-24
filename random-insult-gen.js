d2b848032cde9302d9277b09461740f95406c58a
Ryan Stachelski <41647118+Royal-Crown-Cola@users.noreply.github.com>
2019-01-24 10:17:44 -0700

    Add files via upload
    Added a function to appropriately assign 'a' or 'an' depending on the first letter of a word.

--- random-insult-gen.js
+++ random-insult-gen.js
@@ -6,64 +6,102 @@ var pluralBodyParts = ['legs', 'hands', 'feet', 'arms'];
 var adjectives = ['small', 'weird', 'smelly', 'fat', 'stupid', 
     'lazy', 'boring'];
 
-var words = ['cat', 'poop', 'warthog', 'flea', 'vermin', 'stick', 
+var nouns = ['cat', 'poop', 'warthog', 'flea', 'vermin', 'stick', 
     'fart'];
 
 var names = ['Tom', 'Ben', 'Scott', 'Quinn', 'Niel', 'Mike', 
     'Gudac', 'David', 'Alex', 'Carlos'];
 
-// a function that randomly selects elements from arrays
+// first insult function
 function randomInsultGenerator(name){
 
     // allow user to insult a specific person
     if (!name){
-    var randomName = names[Math.floor(Math.random() * names.length)];
+        var randomName = names[Math.floor(Math.random() * names.length)];
+
     name = randomName
 }
 
     // grab random elements from arrays
     var randomBodyPart = bodyParts[Math.floor(Math.random() * bodyParts.length)];
 
+    var randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+
+    var randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
+
+    return name + ', your ' + randomBodyPart 
+    + ' looks like ' + getIndefinateArticle(randomAdjective) + ' ' + randomAdjective 
+    + ' ' + randomNoun + '!!!';
+}
+
+// second insult function
+function randomInsultGenerator2(name){
+
+    // allow user to insult a specific person
+    if (!name){
+        var randomName = names[Math.floor(Math.random() * names.length)];
+
+        name = randomName
+}
+
+    // grab random elements from arrays
     var pluralRandomBodyPart = pluralBodyParts[Math.floor(Math.random() *
-     pluralBodyParts.length)]
+        pluralBodyParts.length)];
 
     var randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
 
-    var randomWord = words[Math.floor(Math.random() * words.length)];
+    var randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
 
-    return name + ', your ' + randomBodyPart 
-    + ' looks like a ' + randomAdjective + ' ' + randomWord + '!!!';
+    return name + ', your ' + pluralRandomBodyPart 
+    + ' look like ' + getIndefinateArticle(randomAdjective) + ' ' + randomAdjective 
+    + ' ' + randomNoun + '!!!';
+}
+
+// checks the first letter of a string for vowels
+function getIndefinateArticle(word){
+
+    var letter = word.charAt(0);
+    switch(letter){
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+        return 'an';
+        default: 
+        return 'a';
+    }
+
 }
 
 function addBodyPart(newBodyPart){
 
-    bodyParts.push(newBodyPart);
+bodyParts.push(newBodyPart);
 }
 
 function addPluralBodyPart(newPluralBodyPart){
 
-    pluralBodyParts.push(newPluralBodyPart);
+pluralBodyParts.push(newPluralBodyPart);
 }
 
 function addAdjective(newAdjective){
 
-    adjectives.push(newAdjective);
+adjectives.push(newAdjective);
 }
 
 function addWord(newWord){
 
-    words.push(newWord);
+words.push(newWord);
 }
 
 function addName(newName){
 
-    names.push(newName);
+names.push(newName);
 }
 
+var randomRandomInsult = [randomInsultGenerator, randomInsultGenerator2];
 
-//console.log(insult);
-// Write something to check for the adjective starting with a vowel for a/an
-// Write something to check for plurality for look/looks
+var insult = randomRandomInsult[Math.floor(Math.random() * randomRandomInsult.length)];
 
 module.exports = {
     randomInsultGenerator:randomInsultGenerator,
